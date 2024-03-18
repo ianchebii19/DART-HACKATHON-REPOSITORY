866c578ab5b2e86b3530bd20e01284db7be6c73b
Ian Chebii <ianchebii19@gmail.com>
2024-03-18 13:00:43 +0300

    commiit dart files

--- functions.dart
+++ functions.dart
@@ -1,2 +1,30 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+import 'dart:io';
+void main() {
+  double num1 = askForNumber("Enter first number: ");
+  double num2 = askForNumber("Enter second number: ");
+  
+  double sum = addNumbers(num1, num2);
+  double difference = subtractNumbers(num1, num2);
+  
+  print("The sum of the numbers is: $sum");
+  print("The difference of the numbers is: $difference");
+}
+
+double askForNumber(String message) {
+  print(message);
+  double number = double.parse(readLineSync()!); 
+  return number;
+}
+
+double addNumbers(double num1, double num2) {
+  return num1 + num2;
+}
+
+double subtractNumbers(double num1, double num2) {
+  return num1 - num2;
+}
+
+String? readLineSync() {
+ 
+  return stdin.readLineSync();
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,9 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+void main() {
+  String name = "Ian Chebii";
+  int age = 24;
+  String school = "Power Learn Academy";
+  String hobby = "Watching";
+
+  print(
+      "My name is $name, I am $age years old, I go to $school, and I enjoy $hobby.");
+}
